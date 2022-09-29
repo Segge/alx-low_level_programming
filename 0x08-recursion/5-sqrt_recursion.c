bf968fc56abfa812bcbc725f416dc7d143786270
root <root@SeggeLap.localdomain>
2022-09-29 09:15:24 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,10 +18,14 @@ int _sqrt_recursion(int n);
 int find_sqrt(int num, int root)
 {
 	if ((root * root) == num)
+	{
 		return (root);
+	}
 
-	if (root == num / 2)
+	else if (root == num / 2)
+	{
 		return (-1);
+	}
 
 	return (find_sqrt(num, root + 1));
 }
@@ -40,10 +44,13 @@ int _sqrt_recursion(int n)
 	int root = 0;
 
 	if (n < 0)
+	{
 		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
 
-		if (n == 1)
-			return (1);
-
-			return (find_sqrt(n, root));
+		return (find_sqrt(n, root));
 }
