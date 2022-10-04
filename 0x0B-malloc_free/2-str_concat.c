7c1f8664f871cceb1da942d57c805e66a589923b
root <root@SeggeLap.localdomain>
2022-10-04 02:44:36 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,15 +3,15 @@
 /**
  * str_concat - Concatenates two strings
  * @s1: A string to be concatenated
- * s2: A string to be concatenated
+ * @s2: A string to be concatenated
  * Return: returns pointer to newly allocated memory
  * which equal to @s1 + @s2 (SUCCESS) OR
  * NULL (FAILURE)
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int s1_length = 0;
-	unsigned int s2_length = 0;
+	unsigned int s1_len = 0;
+	unsigned int s2_len = 0;
 	char *alloc_mem;
 	unsigned int i, j;
 
@@ -24,28 +24,27 @@ char *str_concat(char *s1, char *s2)
 		{
 			s2 = "";
 		}
-		while (*(s1 + s1_length) != '\0')
+		while (*(s1 + s1_len) != '\0')
 		{
-			s1_length++;
-
-	while (*(s2 + s2_length) != '\0')
-	{
-		s2_length++;
-	}
-	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
-		if (alloc_mem == NULL)
-		{
-			return ('\0');
+			s1_len++;
 		}
-		for (i = 0; i < s1_length; i++)
+		while (*(s2 + s2_len) != '\0')
 		{
-			alloc_mem[i] = *(s1 + i);
+			s2_len++;
 		}
-
-		for (j = 0; j < s2_length; j++, i++)
-		{
-			alloc_mem[i] = *(s2 + j);
-		}
+		alloc_mem = malloc(1 + (s1_len * sizeof(*s1)) + (s2_len * sizeof(*s2)));
+			if (alloc_mem == NULL)
+			{
+				return ('\0');
+			}
+			for (i = 0; i < s1_len; i++)
+			{
+				alloc_mem[i] = *(s1 + i);
+			}
+				for (j = 0; j < s2_len; j++, i++)
+				{
+					alloc_mem[i] = *(s2 + j);
+				}
 
 	return (alloc_mem);
 }
