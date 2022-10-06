0160a500fd1c31439b9cbe32e724380076dff5f7
root <root@SeggeLap.localdomain>
2022-10-06 23:52:11 +0100

    3-array_range.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
