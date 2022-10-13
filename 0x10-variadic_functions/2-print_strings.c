3468419b6b1cd9862e5ec7286f1a38db56e93b7d
root <root@SeggeLap.localdomain>
2022-10-13 19:26:42 +0100

    fix 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,13 +1,41 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * main - check the code for ALX School students.
+ *print_strings - function that Prints strings, followed by a new line.
+ *@separator: string to be printed between strings.
+ *@n: number of strings passed to the function.
+ *@...: variable number of strings to be printed.
  *
- * Return: Always 0.
+ *Description: If separator is NULL, it is not printed.
+ *If one of the strings if NULL, (nil) is printed instead.
  */
 
-int main(void)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	print_strings(", ", 2, "Jay", "Django");
-	return (0);
+	va_list strings;
+	char *str;
+	unsigned int i;
+
+
+	va_start(strings, n);
+
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strings, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+	else
+		printf("%s", str);
+
+	if (i != (n - 1) && separator != NULL)
+		printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(strings);
 }
