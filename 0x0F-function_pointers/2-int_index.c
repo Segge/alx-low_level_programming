c13f3ac657aa6b33560fe0425567487698eac2ee
root <root@SeggeLap.localdomain>
2022-10-13 04:36:02 +0100

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,17 @@
  *the cmp function does not return 0.
  */
 
- int int_index(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i; /* index */
 
+
 	if (array == NULL || cmp == NULL)
-	return (-1);
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-	if (cmp(array[i]) != 0)
+		if (cmp(array[i]) != 0)
 		return (i);
 	}
 
