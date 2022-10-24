8127ef04c93fca8a55389a9059d7deca08e76389
Segge <seggbenga@gmail.com>
2022-10-25 00:27:28 +0100

    add files

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,7 +12,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *elements = h;
 	size_t nodeNumb = 0;
 
-	while (cursor != NULL)
+	while (elements != NULL)
 	{
 		printf("%d\n", elements->n);
 		nodeNumb += 1;
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,7 +10,7 @@
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *nodcount = h;
-	size_t nodenumb = 0;
+	size_t elenumb = 0;
 
 	while (nodcount != NULL)
 	{
