15f2babf21d0b51ae821f91add58186248a91a53
Segge <seggbenga@gmail.com>
2022-10-25 09:42:50 +0100

    add files

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+/**
+ * free_listint2 - frees the linked list
+ * @head : pointer to list
+ * Return: returns void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+	return;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	head = NULL;
+}
